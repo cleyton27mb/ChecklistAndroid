8f9de55a31a29cfb31c25a2de91d0eb1704cd681
cleyton luiz <cleyton27mb@gmail.com>
2017-09-17 11:42:33 -0400

    Implementando a troca de telas

--- app/src/main/java/br/com/alura/checklist/Formulario.java
+++ app/src/main/java/br/com/alura/checklist/Formulario.java
@@ -1,5 +1,6 @@
 package br.com.alura.checklist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,7 @@ public class Formulario extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(Formulario.this, "Tarefa Salva!", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
     }
