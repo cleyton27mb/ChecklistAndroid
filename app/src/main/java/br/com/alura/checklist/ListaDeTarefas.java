ae5a02da23be6558cebdf363bb68034bb79a49af
cleyton luiz <cleyton27mb@gmail.com>
2017-09-17 11:21:18 -0400

    Implementando o botao de nova tarefa

--- app/src/main/java/br/com/alura/checklist/ListaDeTarefas.java
+++ app/src/main/java/br/com/alura/checklist/ListaDeTarefas.java
@@ -2,6 +2,8 @@ package br.com.alura.checklist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaDeTarefas extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ListaDeTarefas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_tarefas);
+
+        String[] tarefas = {"Lavar louça", "Limpar a casa", "Alimentar o cachorro","Lavar louça", "Limpar a casa", "Alimentar o cachorro"
+        ,"Lavar louça", "Limpar a casa", "Alimentar o cachorro","Lavar louça", "Limpar a casa", "Alimentar o cachorro"};
+        ListView listaDeTarefas = (ListView) findViewById(R.id.lista_tarefas);
+        ArrayAdapter<String> adapter = new ArrayAdapter (this,android.R.layout.simple_list_item_1, tarefas);
+        listaDeTarefas.setAdapter(adapter);
     }
 }
