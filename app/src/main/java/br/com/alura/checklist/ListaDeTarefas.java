fff7b9cede2a87c49b6d74a7963d7059bbb54a4d
cleyton luiz <cleyton27mb@gmail.com>
2017-09-20 07:45:37 -0400

    Personalzando botao de salvar

--- app/src/main/java/br/com/alura/checklist/ListaDeTarefas.java
+++ app/src/main/java/br/com/alura/checklist/ListaDeTarefas.java
@@ -1,8 +1,11 @@
 package br.com.alura.checklist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaDeTarefas extends AppCompatActivity {
@@ -17,5 +20,14 @@ public class ListaDeTarefas extends AppCompatActivity {
         ListView listaDeTarefas = (ListView) findViewById(R.id.lista_tarefas);
         ArrayAdapter<String> adapter = new ArrayAdapter (this,android.R.layout.simple_list_item_1, tarefas);
         listaDeTarefas.setAdapter(adapter);
+
+        Button novaTarefa = (Button) findViewById(R.id.nova_tarefa);
+        novaTarefa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentVaiProFormulario = new Intent(ListaDeTarefas.this, Formulario.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
     }
 }
