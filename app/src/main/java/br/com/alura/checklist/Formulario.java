6dddc0903db72cc76084e8b5e2b9e61585025a6e
cleyton luiz <cleyton27mb@gmail.com>
2017-09-20 08:15:04 -0400

    Ajustando bug da classe Formulario para vizualização dos campos

--- app/src/main/java/br/com/alura/checklist/Formulario.java
+++ app/src/main/java/br/com/alura/checklist/Formulario.java
@@ -1,10 +1,12 @@
 package br.com.alura.checklist;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,15 +17,6 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
-
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(Formulario.this, "Tarefa Salva!", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
     }
 
     @Override
@@ -33,5 +26,17 @@ public class Formulario extends AppCompatActivity {
 
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+             Toast.makeText(Formulario.this, "Tarefa Salva!", Toast.LENGTH_SHORT).show();
+            finish();
+             break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
